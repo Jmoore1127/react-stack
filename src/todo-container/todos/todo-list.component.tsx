ad3f4613893f175778564b4da6a7c66aa982dc08
Jared Moore <JMoore@dealersocket.com>
2017-01-24 17:52:59 -0700

    fixed fetch todos

--- src/todo-container/todos/todo-list.component.tsx
+++ src/todo-container/todos/todo-list.component.tsx
@@ -8,12 +8,12 @@ const styles: any = require('./todo-list.component.scss');
 export const TodoLists: React.StatelessComponent<{lists: ITodoList[]}> =
   ({lists}) => {
     return (
-          <List className={styles.todoMasterList}>
-            {
-              _.map(lists, (list: ITodoList) => {
-                return (<Link key={list.id} to={`/todo/${list.id}`}><ListItem  primaryText={list.name}
-                                                                               secondaryText={list.items.length + " items"}></ListItem></Link>);
-              })
-            }
-          </List>)
+      <List className={styles.todoMasterList}>
+        {
+          _.map(lists, (list: ITodoList) => {
+            return (<Link key={list.id} to={`/todo/${list.id}`}><ListItem primaryText={list.name}
+                                                                          secondaryText={list.items.length + " items"}></ListItem></Link>);
+          })
+        }
+      </List>)
   };
