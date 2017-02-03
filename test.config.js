b177a7af9fd9357881491ed844c19215adf55b41
Jared Moore <JMoore@dealersocket.com>
2017-02-03 13:42:28 -0700

    wip setup mocha

---
+++ test.config.js
@@ -0,0 +1,23 @@
+require('babel-register')();
+let chai = require('chai');
+chai.use(require('sinon-chai'));
+require('mocha-sinon')();
+
+let jsdom = require('jsdom').jsdom;
+
+let exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+    if (typeof global[property] === 'undefined') {
+        exposedProperties.push(property);
+        global[property] = document.defaultView[property];
+    }
+});
+
+global.navigator = {
+    userAgent: 'node.js'
+};
+
+documentRef = document;
